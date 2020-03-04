456423c084f9f2921f511afc63e766dc57f1961e
John Telban <Jtelban1@gmail.com>
2020-03-04 08:45:28 -0700

    update on app.js

--- app.js
+++ app.js
@@ -4,33 +4,59 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-console.log(new.Manager)
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const managerQuestion = {
+    name: 'officeNumber',
+    message: "What's the manager's office number?"
+};
+const internQuestion = {
+    name: 'school',
+    message: "What's the intern's school?"
+};
+const engineerQuestion = {
+    name: 'github',
+    message: "What's the engineer's github account?"
+};
+
+
+const questions = [
+    {
+        name: 'name',
+        message: 'What\'s the name of your employee'
+    },
+    {
+        name: 'id',
+        message: "The employee's id?",
+
+    },
+    {
+        name: "email",
+        message: "The employee's email is?"
+    }
+];
+const addEmployee = [
+    {
+        name: 'add',
+        message: 'Do you want to add another team member?',
+        type: 'confirm'
+    },
+    {
+        name: 'employeeType',
+        message: 'What kind of employee do you want to add?',
+        type: 'list',
+        choices: ['Intern', 'Engineer'],
+        when: function(responses){
+            return responses.add;
+        }
+    }
+];
+
+let manager = questions;
+manager.push(managerQuestion);
+console.log(manager);
